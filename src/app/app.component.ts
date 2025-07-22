a673f585045f31b3993c762273adea8e95ce55a8
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2025-07-22 12:30:57 +0330

    register user service side effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { PageRouterOutlet } from "@nativescript/angular";
 import { MessageService } from "./core/services/message.service";
 import { AppLinkService } from "./core/services/app-link.service";
 import { AuthService } from "./core/services/auth.service";
+import { UserService } from "./core/services/user.service";
 
 @Component({
   selector: "ns-app",
@@ -19,10 +20,14 @@ import { AuthService } from "./core/services/auth.service";
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild("messageAnchor", { read: ViewContainerRef }) messageAnchorRef: ViewContainerRef;
+  @ViewChild("messageAnchor", { read: ViewContainerRef })
+  messageAnchorRef: ViewContainerRef;
   messageService = inject(MessageService);
   appLinkService = inject(AppLinkService);
   authService = inject(AuthService);
+
+  // Inject to register app start side effects
+  userService = inject(UserService);
 
   ngOnInit(): void {
     this.appLinkService.registerHandler();
