f9626c968e8bb66202cac154cb1e31141947548b
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2025-11-02 19:47:36 +0330

    Add base item page component
    #34

---
+++ src/app/features/items/base-item-page.component.ts
@@ -0,0 +1,112 @@
+import {
+  Directive,
+  OnDestroy,
+  OnInit,
+  ViewContainerRef,
+  WritableSignal,
+  computed,
+  inject,
+  signal,
+} from "@angular/core";
+import { StateService } from "~/app/core/services/state.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MessageService } from "~/app/core/services/message.service";
+import { NeoDBLocalizePipe } from "~/app/shared/pipes/neodb-localize.pipe";
+import { Location } from "@angular/common";
+import { PageTransition, SharedTransition } from "@nativescript/core";
+import { shareText } from "@nativescript/social-share";
+import { PostsStateService } from "~/app/features/posts/posts-state.service";
+import { PostEditorsService } from "~/app/features/posts/editors/post-editors.service";
+import {
+  BottomSheetOptions,
+  BottomSheetService,
+} from "@nativescript-community/ui-material-bottomsheet/angular";
+import { BaseItem } from "~/app/core/models/base-item.model";
+import { AddToCollectionComponent } from "./add-to-collection/add-to-collection.component";
+
+@Directive()
+export abstract class BaseItemPageComponent implements OnInit, OnDestroy {
+  protected bottomSheet = inject(BottomSheetService);
+  stateService = inject(StateService);
+  messageService = inject(MessageService);
+  postsStateService = inject(PostsStateService);
+  postEditorsService = inject(PostEditorsService);
+  activatedRoute = inject(ActivatedRoute);
+  location = inject(Location);
+  router = inject(Router);
+  containerRef = inject(ViewContainerRef);
+  neoL = inject(NeoDBLocalizePipe);
+  statusbarSize: number = global.statusbarSize;
+  pageLoading = signal(false);
+  descriptionCollapsed = signal(true);
+  itemTitle = computed(() =>
+    this.item()
+      ? this.neoL.transform(
+          this.item().localizedTitle,
+          this.stateService.preference().language,
+        ) ||
+        this.item().displayTitle ||
+        this.item().title
+      : undefined,
+  );
+  abstract item: WritableSignal<BaseItem>;
+
+  ngOnInit(): void {
+    const uuid = this.activatedRoute.snapshot.params.uuid;
+    this.getItemDetails(uuid);
+    this.postsStateService.getPostsForItem(uuid);
+  }
+
+  abstract getItemDetails(uuid: string);
+
+  share() {
+    shareText(this.stateService.instanceURL() + this.item().url);
+  }
+
+  navigateToCollection(event: any) {
+    const item = event.item;
+    this.router.navigate([`/collections/` + item.uuid], {
+      transition: SharedTransition.custom(new PageTransition(), {
+        pageReturn: {
+          duration: 150,
+        },
+      }),
+    } as any);
+  }
+
+  showCollections() {
+    const options: BottomSheetOptions = {
+      viewContainerRef: this.containerRef,
+      context: { itemUUID: this.item().uuid },
+      dismissOnDraggingDownSheet: false,
+      transparent: true,
+    };
+
+    this.bottomSheet.show(AddToCollectionComponent, options).subscribe();
+  }
+
+  showAllPosts(type: string) {
+    this.router.navigate([`/posts`], {
+      queryParams: this.getPostsQueryParams(type),
+    });
+  }
+
+  navigateToPost(postId: string, type: string) {
+    this.router.navigate([`/posts/${postId}`], {
+      queryParams: this.getPostsQueryParams(type),
+    });
+  }
+
+  getPostsQueryParams(type: string) {
+    return {
+      type,
+      itemUUID: this.item().uuid,
+      itemCategory: this.item().category,
+      itemTitle: this.itemTitle(),
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.postsStateService.loadPreviousItemPosts(this.item().uuid);
+  }
+}
--- src/app/features/items/movie/movie.component.ts
+++ src/app/features/items/movie/movie.component.ts
@@ -4,8 +4,6 @@ import {
   NO_ERRORS_SCHEMA,
   OnDestroy,
   OnInit,
-  ViewContainerRef,
-  computed,
   inject,
   signal,
 } from "@angular/core";
@@ -15,27 +13,20 @@ import {
   NativeScriptRouterModule,
 } from "@nativescript/angular";
 import { CollectionViewModule } from "@nativescript-community/ui-collectionview/angular";
-import { StateService } from "../../../core/services/state.service";
 import { MovieService } from "../../../core/services/movie.service";
 import { NativeScriptLocalizeModule } from "@nativescript/localize/angular";
 import { CollectionItemComponent } from "../../../shared/components/items/collection-item/collection-item.component";
 import { finalize } from "rxjs";
-import { ActivatedRoute, Router } from "@angular/router";
 import { Movie } from "~/app/core/models/movie.model";
-import { MessageService } from "~/app/core/services/message.service";
 import { localize } from "@nativescript/localize";
 import { NeoDBLocalizePipe } from "~/app/shared/pipes/neodb-localize.pipe";
 import { KiloPipe } from "~/app/shared/pipes/kilo.pipe";
-import { Location } from "@angular/common";
 import { RateIndicatorComponent } from "~/app/shared/components/rate-indicator/rate-indicator.component";
 import { IconTextButtonComponent } from "~/app/shared/components/icon-text-button/icon-text-button.component";
 import { ExternalResourcesComponent } from "~/app/shared/components/external-resources/external-resources.component";
 import { RatingsChartComponent } from "~/app/shared/components/ratings-chart/ratings-chart.component";
 import { PostItemComponent } from "~/app/shared/components/post/post-item/post-item.component";
-import { PageTransition, SharedTransition } from "@nativescript/core";
-import { shareText } from "@nativescript/social-share";
-import { PostsStateService } from "~/app/features/posts/posts-state.service";
-import { PostEditorsService } from "~/app/features/posts/editors/post-editors.service";
+import { BaseItemPageComponent } from "../base-item-page.component";
 
 @Component({
   selector: "ns-movie",
@@ -59,45 +50,21 @@ import { PostEditorsService } from "~/app/features/posts/editors/post-editors.se
   schemas: [NO_ERRORS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovieComponent implements OnInit, OnDestroy {
-  stateService = inject(StateService);
+export class MovieComponent extends BaseItemPageComponent implements OnInit, OnDestroy {
   movieService = inject(MovieService);
-  messageService = inject(MessageService);
-  postsStateService = inject(PostsStateService);
-  postEditorsService = inject(PostEditorsService);
-  activatedRoute = inject(ActivatedRoute);
-  location = inject(Location);
-  router = inject(Router);
-  containerRef = inject(ViewContainerRef);
-  neoL = inject(NeoDBLocalizePipe);
-  statusbarSize: number = global.statusbarSize;
-  pageLoading = signal(false);
-  movie = signal<Movie>(null);
-  descriptionCollapsed = signal(true);
-  itemTitle = computed(() =>
-    this.movie()
-      ? this.neoL.transform(
-          this.movie().localizedTitle,
-          this.stateService.preference().language,
-        ) ||
-        this.movie().displayTitle ||
-        this.movie().title
-      : undefined,
-  );
+  item = signal<Movie>(null);
 
   ngOnInit(): void {
-    const uuid = this.activatedRoute.snapshot.params.uuid;
-    this.getMovieDetails(uuid);
-    this.postsStateService.getPostsForItem(uuid);
+    super.ngOnInit();
   }
 
-  getMovieDetails(uuid: string) {
+  getItemDetails(uuid: string) {
     this.pageLoading.set(true);
     this.movieService
       .getMovieDetails(uuid)
       .pipe(finalize(() => this.pageLoading.set(false)))
       .subscribe({
-        next: (movie) => this.movie.set(movie),
+        next: (movie) => this.item.set(movie),
         error: (err) =>
           this.messageService.showErrorMessage(
             localize("common.generic_connection_error"),
@@ -105,47 +72,7 @@ export class MovieComponent implements OnInit, OnDestroy {
       });
   }
 
-  share() {
-    shareText(this.stateService.instanceURL() + this.movie().url);
-  }
-
-  navigateToCollection(event: any) {
-    const item = event.item;
-    this.router.navigate([`/collections/` + item.uuid], {
-      transition: SharedTransition.custom(new PageTransition(), {
-        pageReturn: {
-          duration: 150,
-        },
-      }),
-    } as any);
-  }
-
-  showCollections() {
-    // TODO: Mohammad 09-09-2025: Implement it
-  }
-
-  showAllPosts(type: string) {
-    this.router.navigate([`/posts`], {
-      queryParams: this.getPostsQueryParams(type),
-    });
-  }
-
-  navigateToPost(postId: string, type: string) {
-    this.router.navigate([`/posts/${postId}`], {
-      queryParams: this.getPostsQueryParams(type),
-    });
-  }
-
-  getPostsQueryParams(type: string) {
-    return {
-      type,
-      itemUUID: this.movie().uuid,
-      itemCategory: this.movie().category,
-      itemTitle: this.itemTitle(),
-    };
-  }
-
   ngOnDestroy(): void {
-    this.postsStateService.loadPreviousItemPosts(this.movie().uuid);
+    super.ngOnDestroy();
   }
 }
