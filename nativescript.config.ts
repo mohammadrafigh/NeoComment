6ec60693bd0585920049580d97b5cb168089a330
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2025-11-19 20:57:04 +0330

    Add more production configs
    #46

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,5 +1,9 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
+// TODO: Mohammad 11-19-2025: It would be different when we switch to Vite
+const NS_ENV = process.env.NATIVESCRIPT_WEBPACK_ENV ?? '{}';
+const isProduction = JSON.parse(NS_ENV).production;
+
 export default {
   id: 'com.mohammadrafigh.neocomment',
   appPath: 'src',
@@ -7,6 +11,7 @@ export default {
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none',
-    maxLogcatObjectSize: 2048,
+    maxLogcatObjectSize: isProduction ? 1024 : 2048,
+    discardUncaughtJsExceptions: isProduction,
   }
 } as NativeScriptConfig;
